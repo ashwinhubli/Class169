c652686ccbdb99c7ad0d8d0c308b5feec62ca771
ashwinhubli <66745007+ashwinhubli@users.noreply.github.com>
2021-12-18 19:58:29 +0530

    Add files via upload

---
+++ js/MarkerHandler.js
@@ -0,0 +1,41 @@
+AFRAME.registerComponent("marker-handler",{
+  init:async function(){
+   this.el.addEventListener("markerFound",()=>{
+    console.log("marker is found")
+    this.handleMarkerFound()   
+   })
+   this.el.addEventListener("markerLost",()=>{
+     console.log("marker is lost")
+     this.handleMarkerLost  
+   })  
+},
+   handleMarkerFound:function(){
+    var buttonDiv = document.getElementById("button-div")
+    buttonDiv.style.display = "flex"
+    
+    var ratingButton = document.getElementById("rating-button")
+    var orderButton = document.getElementById("order-button")
+
+    ratingButton.addEventListener("click",function(){
+     swal({
+      icon:"warning",
+      title:"rate this",
+      text:"work in progress"   
+     })   
+    })
+
+    orderButton.addEventListener("click",()=>{
+     swal({
+      icon:"https://i.imgur.com/4NZ6uLY.jpg",
+      title:"Thanks for the order",
+      text:"Your order will be served soon at your table"
+     })   
+    })
+   },
+   handleMarkerLost:function(){
+    var buttonDiv = document.getElementById("button-div")
+    buttonDiv.style.display = "none"
+    
+   }
+    
+})
